c87b33d071102c82a05c8f31c335f581ffac244e
DebbDev-cloud <isholadeborahope@gmail.com>
2021-11-08 15:53:43 +0100

    make the js file external instead of internal

---
+++ js/main.js
@@ -0,0 +1,24 @@
+function addition() {
+    input1 = parseInt(myForm.input1.value);
+    input2 = parseInt(myForm.input2.value);
+    answer = input1 + input2;
+    myForm.answer.value = answer;
+}
+function substraction() {
+    input1 = parseInt(myForm.input1.value);
+    input2 = parseInt(myForm.input2.value);
+    answer = input1 + input2;
+    myForm.answer.value = answer;
+}
+function multiply() {
+    input1 = parseInt(myForm.input1.value);
+    input2 = parseInt(myForm.input2.value);
+    answer = input1 + input2;
+    myForm.answer.value = answer;
+}
+function division() {
+    input1 = parseInt(myForm.input1.value);
+    input2 = parseInt(myForm.input2.value);
+    answer = input1 + input2;
+    myForm.answer.value = answer;
+}
